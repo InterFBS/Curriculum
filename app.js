bf1f8ea27ea7376883b412e79eeac5995a6d4f49
FelipeBarroso <felipebarroso299@gmail.com>
2022-04-07 23:05:30 -0300

    Mudança para aparecer o CV no Menu

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const menuHamburger = document.querySelector('.menu-hamburger')
 const navMenu = document.querySelector('.nav-menu')
-
+const buttonCv = document.querySelector('#buttonHidden')
 
 menuHamburger.addEventListener('click', () => {
     menuHamburger.classList.toggle('active')
     navMenu.classList.toggle('active')
+    buttonCv.classList.toggle('active')
 })
